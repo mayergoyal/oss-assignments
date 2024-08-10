a10b8bb066279866c8d6c8cfd14272689975c87c
MAYER GOYAL <144149330+mayergoyal@users.noreply.github.com>
2024-08-10 10:41:25 +0530

    Create setB_qusetion2.cpp

---
+++ setB_qusetion2.cpp
@@ -0,0 +1,25 @@
+#include <bits//stdc++.h>
+using namespace std;
+bool isprime(int n){
+for(int i=3;i<n;i++){
+    if(n%i==0)return false;
+}
+return true;
+}
+int main(){
+cout<<"enter the number";
+int n;
+cin>>n;
+if (n==2) {
+    cout<<1<< 2;
+    return 0;
+}
+vector<int>ans;
+for(int i=3;i<n;i++)
+if(isprime(i)){
+    ans.push_back(i);
+}
+for(auto i :ans){
+    cout<<i<<" ";
+}
+}
